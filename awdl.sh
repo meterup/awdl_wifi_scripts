49d17d11fcf8e6a6a94b45239b0f6cccb2407e67
Abhijeet Kumar <abhijeet@meter.com>
2022-12-06 15:58:47 -0800

    Sudo fixes

--- awdl.sh
+++ awdl.sh
@@ -3,14 +3,14 @@
 read -p "See https://www.meter.com/mac_osx_awdl_psa for more details. You will be prompted for your admin password. Do you want to continue? y/n" choice
 
 case $choice in
-[yY]* ) echo "Installing ventura_disable_awdl.sh in your home directory. This will run in the background." ;;
+[yY]* ) sudo echo "Installing ventura_disable_awdl.sh in your home directory. This will run in the background." ;;
 [nN]* ) exit ;;
 *) exit ;;
 esac
 
-curl -s https://raw.githubusercontent.com/meterup/awdl_wifi_scripts/ventura_disable_awdl.sh > ~/ventura_disable_awdl.sh
+curl -s https://raw.githubusercontent.com/meterup/awdl_wifi_scripts/main/ventura_disable_awdl.sh > ~/ventura_disable_awdl.sh
 chmod u+x ~/ventura_disable_awdl.sh
 
-sudo ~/./ventura_disable_awdl.sh > /dev/null &
+sudo bash ~/ventura_disable_awdl.sh > /dev/null &
 
 rm ~/ventura_disable_awdl.sh
