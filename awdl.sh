35838ac7ccb49df71cb7011bc0b92a50ba2cec5b
Eric Cheng <85464632+ericmeter@users.noreply.github.com>
2022-12-06 14:07:23 -0800

    Update awdl.sh
    Formatting nits, no functional changes

--- awdl.sh
+++ awdl.sh
@@ -1,18 +1,14 @@
 #!/usr/bin/env bash
 
-read -p "See https://www.meter.com/mac_osx_awdl_psa for more details. You will be prompted for your admin password. Do you want to continue?  y/n/c " choice
+read -p "See https://www.meter.com/mac_osx_awdl_psa for more details. You will be prompted for your admin password. Do you want to continue? y/n" choice
 
 case $choice in
-[yY]* ) echo "Great" ;;
+[yY]* ) echo "Installing ventura_disable_awdl.sh in your home directory. This will run in the background." ;;
 [nN]* ) exit ;;
-[cC]* ) exit ;;
 *) exit ;;
 esac
-
-sudo echo "Installing ventura_disable_awdl.sh in your home directory. This will run in background."
 
 curl -s https://raw.githubusercontent.com/meterup/awdl_wifi_scripts/ventura_disable_awdl.sh > ~/ventura_disable_awdl.sh
 chmod u+x ~/ventura_disable_awdl.sh
 
 sudo ~/./ventura_disable_awdl.sh > /dev/null &
-
