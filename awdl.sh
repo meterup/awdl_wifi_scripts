32d121346aacdc9868a3423930b85ba01e1413d0
Abhijeet Kumar <abhijeet@meter.com>
2022-12-06 16:03:52 -0800

    move to tmp dir instead of home

--- awdl.sh
+++ awdl.sh
@@ -3,12 +3,12 @@
 read -p "See https://www.meter.com/mac_osx_awdl_psa for more details. You will be prompted for your admin password. Do you want to continue? y/n" choice
 
 case $choice in
-[yY]* ) sudo echo "Installing ventura_disable_awdl.sh in your home directory. This will run in the background." ;;
+[yY]* ) sudo echo "Installing ventura_disable_awdl.sh in your tmp directory. This will run in the background." ;;
 [nN]* ) exit ;;
 *) exit ;;
 esac
 
-curl -s https://raw.githubusercontent.com/meterup/awdl_wifi_scripts/main/ventura_disable_awdl.sh > ~/ventura_disable_awdl.sh
-chmod u+x ~/ventura_disable_awdl.sh
+curl -s https://raw.githubusercontent.com/meterup/awdl_wifi_scripts/main/ventura_disable_awdl.sh > /tmp/ventura_disable_awdl.sh
+chmod u+x /tmp/ventura_disable_awdl.sh
 
-sudo bash ~/ventura_disable_awdl.sh > /dev/null &
+sudo bash /tmp/ventura_disable_awdl.sh > /dev/null &
