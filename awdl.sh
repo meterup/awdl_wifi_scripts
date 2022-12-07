cada3bda91c5fa7450a53e89f11c5bb461f2b736
Abhijeet Kumar <abhijeet@meter.com>
2022-12-07 07:06:23 -0800

    Replace underscore with hyphen

--- awdl.sh
+++ awdl.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-read -p "See https://www.meter.com/mac_osx_awdl_psa for more details. You will be prompted for your admin password. Do you want to continue? y/n" choice
+read -p "See https://www.meter.com/mac-osx-awdl-psa for more details. You will be prompted for your admin password. Do you want to continue? y/n" choice
 
 case $choice in
 [yY]* ) sudo echo "Installing ventura_disable_awdl.sh in your tmp directory. This will run in the background." ;;
