e870d4940d634d681b25aa102c3e95f01f21625f
Abhijeet Kumar <abhijeet@meter.com>
2022-12-07 21:10:33 -0800

    Drop ventura from the file name

--- awdl.sh
+++ awdl.sh
@@ -3,12 +3,12 @@
 read -p "See https://www.meter.com/mac-osx-awdl-psa for more details. You will be prompted for your admin password. Do you want to continue? y/n" choice
 
 case $choice in
-[yY]* ) sudo echo "Installing ventura_disable_awdl.sh in your tmp directory. This will run in the background." ;;
+[yY]* ) sudo echo "Installing disable_awdl.sh in your tmp directory. This will run in the background." ;;
 [nN]* ) exit ;;
 *) exit ;;
 esac
 
-curl -s https://raw.githubusercontent.com/meterup/awdl_wifi_scripts/main/ventura_disable_awdl.sh > /tmp/ventura_disable_awdl.sh
-chmod u+x /tmp/ventura_disable_awdl.sh
+curl -s https://raw.githubusercontent.com/meterup/awdl_wifi_scripts/main/disable_awdl.sh > /tmp/disable_awdl.sh
+chmod u+x /tmp/disable_awdl.sh
 
-sudo bash /tmp/ventura_disable_awdl.sh > /dev/null &
+sudo bash /tmp/disable_awdl.sh > /dev/null &
