9f8aa78b7feaca97abf4dd5d7b461bb07f24f1bc
Abhijeet Kumar <abhijeet@meter.com>
2022-12-09 09:43:47 -0800

    Remove extraneous rm

--- awdl-daemon.sh
+++ awdl-daemon.sh
@@ -4,6 +4,5 @@ curl -s https://raw.githubusercontent.com/meterup/awdl_wifi_scripts/main/disable
 sudo chmod u+x ~/disable_awdl.sh
 cd /Library/LaunchDaemons/ && sudo curl -sO https://raw.githubusercontent.com/meterup/awdl_wifi_scripts/main/com.meter.wifi.awdl.plist
 sudo sed -i -- "s/YOUR_USERNAME/${USER}/g" /Library/LaunchDaemons/com.meter.wifi.awdl.plist
-rm com.meter.wifi.awdl.plist-- 2> /dev/null
 sudo launchctl unload -w /Library/LaunchDaemons/com.meter.wifi.awdl.plist 2> /dev/null
 sudo launchctl load -w /Library/LaunchDaemons/com.meter.wifi.awdl.plist
