df22ebd4997def723fe2ec0ee7178931efc15a1d
Abhijeet Kumar <abhijeet@meter.com>
2022-12-08 17:08:39 -0800

    refactor and reduce complexity

--- awdl-daemon.sh
+++ awdl-daemon.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-curl -s https://raw.githubusercontent.com/meterup/awdl_wifi_scripts/main/disable_awdl.sh > ~/disable_awdl.sh
-sudo chmod u+x ~/disable_awdl.sh
 sudo curl -s https://raw.githubusercontent.com/meterup/awdl_wifi_scripts/main/com.meter.wifi.awdl.plist > /Library/LaunchDaemons/com.meter.wifi.awdl.plist
-sudo sed -i -- "s/YOUR_USERNAME/${USER}/g" /Library/LaunchDaemons/com.meter.wifi.awdl.plist
-rm com.meter.wifi.awdl.plist-- 2> /dev/null
 sudo launchctl unload -w /Library/LaunchDaemons/com.meter.wifi.awdl.plist || true
 sudo launchctl load -w /Library/LaunchDaemons/com.meter.wifi.awdl.plist
