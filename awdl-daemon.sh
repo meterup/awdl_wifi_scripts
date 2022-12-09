31d9e5827ae1e23f0fa3bfc4d826ce434f02f425
Abhijeet Kumar <abhijeet@meter.com>
2022-12-09 07:51:42 -0800

    Silence stdout and stderr

--- awdl-daemon.sh
+++ awdl-daemon.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 cd /Library/LaunchDaemons/ && sudo curl -sO https://raw.githubusercontent.com/meterup/awdl_wifi_scripts/main/com.meter.wifi.awdl.plist
-sudo launchctl unload -w /Library/LaunchDaemons/com.meter.wifi.awdl.plist || true
+sudo launchctl unload -w /Library/LaunchDaemons/com.meter.wifi.awdl.plist > /dev/null 2>&1
 sudo launchctl load -w /Library/LaunchDaemons/com.meter.wifi.awdl.plist
