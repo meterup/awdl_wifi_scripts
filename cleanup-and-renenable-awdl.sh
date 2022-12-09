2ae98a1a0d2e0b852d35a7292aff1cdbe2d58cd8
Abhijeet Kumar <abhijeet@meter.com>
2022-12-09 09:26:54 -0800

    LaunchDaemon running a script in a loop

--- cleanup-and-renenable-awdl.sh
+++ cleanup-and-renenable-awdl.sh
@@ -4,4 +4,5 @@ sudo launchctl unload -w /Library/LaunchDaemons/com.meter.wifi.awdl.plist
 sudo rm /Library/LaunchDaemons/com.meter.wifi.awdl.plist
 sudo pkill -f /tmp/disable_awdl.sh
 sudo rm /tmp/disable_awdl.sh
+rm ~/disable_awdl.sh
 sudo ifconfig awdl0 up
